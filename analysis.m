dc9248dadc7f252b72c3d6ddc3a73774ecd5f47c
Yama-Hokudai <yshoma@ist.hokudai.ac.jp>
2019-08-02 20:23:15 +0900

    20190802

---
+++ analysis.m
@@ -0,0 +1,64 @@
+c=34000;
+Tx=[0,50;50,50;-50,50];
+sp=[-10,0;10,0];
+ts=zeros(size(Tx)(1)*3,1);
+for k=1:size(Tx)(1)
+  ts(k*3,1) = (norm(Tx(k,:)-sp(1,:)) - norm(Tx(k,:)-sp(2,:)))/c;
+endfor
+ts=round(ts*sampling);
+
+cor=matchedFilter(pilot,data(:,sampling:3*sampling));
+[tmp,ind]=sort(cor,2,"descend");
+
+window=[];
+for k=1:size(data)(1)
+  window=[window;data(k,sampling+ind(k,1)+sampling/1000:sampling+ind(k,1)+3*sampling/1000)];
+endfor
+
+Arg = arg(2*j*window*pilotExp(2,:)')-arg(2*j*window*pilotExp(1,:)');
+for k = 1:length(Arg)
+  if Arg(k) > pi
+    Arg(k) -= 2*pi;
+  elseif Arg(k) <= -pi
+    Arg(k) += 2*pi;
+  endif
+endfor
+for k=1:length(Arg)
+  Arg(k)=sampling*Arg(k)*(1/pi-1/abs(Arg(k)))/1000;
+endfor
+
+index = ind(:,1)+sampling+Arg;
+index = round(index)+ts;
+
+phi1=zeros(9,10);
+phi2=zeros(9,10);
+amp1=zeros(9,10);
+amp2=zeros(9,10);
+
+for k=1:10
+  window=[];
+  for l=1:size(data)(1)
+    window=[window;data(l,index(l)+(150*k+1)*sampling/1000:index(l)+(150*k+3)*sampling/1000)];
+  endfor
+  pro1=matchedFilter(comsin(1,:),window);
+  pro2=matchedFilter(comsin(2,:),window);
+  phi1(:,k) = arg(pro1);
+  phi2(:,k) = arg(pro2);
+  amp1(:,k) = abs(pro1);
+  amp2(:,k) = abs(pro2);
+endfor
+
+Arg=zeros(3,10);
+for k=1:10
+  for l=1:3
+    Arg(l,k) = arg(amp2(3*l-1,k)*exp(j*phi2(3*l-1,k)) + amp2(3*l,k)*exp(j*phi2(3*l,k))) - arg(amp1(3*l-1,k)*exp(j*phi1(3*l-1,k)) + amp1(3*l,k)*exp(j*phi1(3*l,k)));
+    Arg(l,k) = verifying(Arg(l,k));
+  endfor
+endfor
+
+theta = Arg(:,2:end) - Arg(:,1:end-1);
+for k = 1:size(theta)(1)
+  for l = 1:size(theta)(2)
+    theta(k,l) = demodule(6,theta(k,l));
+  endfor
+endfor
--- spotdem.m
+++ spotdem.m
@@ -6,24 +6,44 @@ t2 = -1/1000:1/sampling:3/1000;
 
 comsin = [exp(-j*2*pi*freq(1)*t1);exp(-j*2*pi*freq(2)*t1)];
 sync = syncpattern(freq(1),freq(2),t2,0,0);
-f0 = 12000;
-f1 = 18000;
-k = (f1 - f0)/10/1000;
-t3 = 0:1/sampling:10/1000;
-%t3 += 3/1000;
-Chirp = chirp2(t3 ,f0,f1,10/1000);
+
+pilot = syncpattern(15750,16250,t2,0,0);
+pilotExp= [exp(-j*2*pi*15750*t1);exp(-j*2*pi*16250*t1)];
 
 index = zeros(9,1);
 recsymbol = zeros(9,9,100);
-for iter = 1:1 
+for iter = 1:2
   window=[];
   for k=1:size(data)(1)
     window=[window;data(k,index(k)+sampling:index(k)+3*sampling)];
   endfor
-  tmp = matchedFilter(Chirp,window);
+  
+  tmp = matchedFilter(pilot,window);
   [tmp,ind] = sort(abs(tmp),2,"descend");
-  index += ind(:,1)+sampling;
+  
+  window=[];
+  for k=1:size(data)(1)
+    window=[window;data(k,ind(k,1) + index(k) + round(sampling*(1+1/1000)):ind(k,1) + index(k) + round(sampling*(1+3/1000)))];
+  endfor
+
+  Arg = arg(2*j*window*pilotExp(2,:)')-arg(2*j*window*pilotExp(1,:)');
+
+  for k = 1:length(Arg)
+    if Arg(k) > pi
+      Arg(k) -= 2*pi;
+    elseif Arg(k) <= -pi
+      Arg(k) += 2*pi;
+    endif
+  endfor
+
+  for k=1:length(Arg)
+    Arg(k)=sampling*Arg(k)*(1/pi-1/abs(Arg(k)))/1000;
+  endfor
+
+  index += ind(:,1)+sampling+Arg;
   index += 151*sampling/1000;
+  index = round(index);
+  
   window = [];
   for k = 1:size(data)(1)
     window = [window;data(k,index(k):index(k)+2*sampling/1000)];
@@ -32,34 +52,33 @@ for iter = 1:1
   for k = 1:length(Arg)
     if Arg(k) > pi
       Arg(k) -= 2*pi;
-      elseif Arg(k) <= -pi
-        Arg(k) += 2*pi;
-      endif
-    endfor
-    epoch = index;
-    Arg = [Arg,Arg];
+    elseif Arg(k) <= -pi
+      Arg(k) += 2*pi;       
+    endif
+  endfor
+  epoch = index;
+  Arg = [Arg,Arg];
 
-    seqence = zeros(length(Arg),9);
+  seqence = zeros(length(Arg),9);
     
-      for k = 1:9
-      window = [];
-      for l = 1:size(data)(1)
-        window = [window;data(l,epoch(l) + k*150*sampling/1000:epoch(l) + (2+k*150)*sampling/1000)];
-      endfor
-      Arg(:,1) = arg(2*j*window*comsin(2,:)')-arg(2*j*window*comsin(1,:)');
-      for l = 1:length(Arg)
-          Arg(l,1) = verifying(Arg(l,1));
-        endfor
-        tmp = Arg(:,2) - Arg(:,1);
-        Arg(:,2) = Arg(:,1);
-
-        for l = 1:length(tmp)
-          tmp(l) = verifying(tmp(l));
-          tmp(l) = demodule(m,tmp(l));
-        endfor
-        seqence(:,k) = tmp;
-      endfor
-      recsymbol(:,:,iter) = seqence;
-      disp(iter);
+  for k = 1:9
+    window = [];
+    for l = 1:size(data)(1)
+      window = [window;data(l,epoch(l) + k*150*sampling/1000:epoch(l) + (2+k*150)*sampling/1000)];
     endfor
-    
+    Arg(:,1) = arg(2*j*window*comsin(2,:)')-arg(2*j*window*comsin(1,:)');
+    for l = 1:length(Arg)
+      Arg(l,1) = verifying(Arg(l,1));
+    endfor
+    tmp = Arg(:,2) - Arg(:,1);
+    Arg(:,2) = Arg(:,1);
+
+    for l = 1:length(tmp)
+      tmp(l) = verifying(tmp(l));
+      tmp(l) = demodule(m,tmp(l));
+    endfor
+    seqence(:,k) = tmp;
+  endfor
+  recsymbol(:,:,iter) = seqence;
+  disp(iter);
+endfor
